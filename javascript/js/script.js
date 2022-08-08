fa8bb0cb5ebff8d447c7f7330a0b7af6f0109f17
Raphaelfn <raphael.fn@hotmail.com>
2022-08-08 11:59:03 -0300

    correções

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -2,7 +2,7 @@
 function calcularMedia( notas ) {
     let soma = 0;
     for( c = 0; c < notas.length; c++) {
-        notas += notas[c];
+        soma += notas[c];
     }
 
     media = soma / notas.length;
