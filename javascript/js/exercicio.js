d3a44977dd84ca7808ceb9db01a23260ec436b8f
Raphaelfn <raphael.fn@hotmail.com>
2022-08-10 15:47:46 -0300

    updade exercicio

---
+++ javascript/js/exercicio.js
@@ -0,0 +1,32 @@
+var branco = "preto";
+var preto = "cinza";
+var cinza = "branco";
+var carro = "preto";
+var valor = 30000
+var prestacao = 750;
+var entrada = 3000;
+
+var cavalo = carro == "preto" ? "cinza" : "marron";
+
+if (carro = "preto"){
+cavalo = "preto";
+} else if (carro == "vermelho"){
+cavalo = "cinza";
+} else if(carro == "amarelo"){
+cavalo = "marron";
+} else {
+cavalo = "azul";
+}
+
+var financiamento = (valor - entrada) / prestacao;
+
+
+
+
+console.log(branco == "branco");
+console.log(branco == cinza);
+console.log(carro === branco);
+console.log(cavalo);
+console.log(financiamento);
+
+
